947cc6a046f1a3a139dbc164d53751abc2f276b4
Oscar Persson <dallas.persson@gmail.com>
2015-11-18 19:30:18 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Oscar's dotfiles
+
+## Usage
+Give `install.sh` executed permissions:
+```
+chmod -x ./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Get current dir (so run this script from anywhere)
+
+export DOTFILES_DIR
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $DOTFILES_DIR
+
+# Bunch of symlinks
+
+ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
+ln -sfv "$DOTFILES_DIR/runcom/.inputrc" ~
+ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
