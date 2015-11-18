ebbf97c6fe0519ace42410667045ce02f0732fd8
Oscar Persson <dallas.persson@gmail.com>
2015-11-18 19:49:44 +0100

    Update dotfiles in install script

--- install.sh
+++ install.sh
@@ -4,7 +4,10 @@
 
 export DOTFILES_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo $DOTFILES_DIR
+
+# Update dotfiles itself first
+
+[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
 # Bunch of symlinks
 
